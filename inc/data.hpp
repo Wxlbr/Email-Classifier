015107aa43f2e7e54f01823bf550166df2f3b9b7
Wilbur42 <Will007ward@gmail.com>
2023-09-04 17:10:06 +0100

    Added Dataset Loading

---
+++ inc/data.hpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+struct CSVRecord {
+    std::string text;
+    int label;
+};
+
+std::string cleanText(const std::string& text) {
+    std::string cleanedText = text;
+
+    // Remove all punctuation
+    for (int i = 0; i < cleanedText.size(); i++) {
+        if (ispunct(cleanedText[i])) {
+            cleanedText.erase(i--, 1);
+        }
+    }
+
+    // Convert to lowercase
+    for (int i = 0; i < cleanedText.size(); i++) {
+        cleanedText[i] = tolower(cleanedText[i]);
+    }
+
+    // Convert all whitespace to single spaces
+    for (int i = 0; i < cleanedText.size(); i++) {
+        if (isspace(cleanedText[i])) {
+            while (isspace(cleanedText[i + 1])) {
+                cleanedText.erase(i + 1, 1);
+            }
+        }
+    }
+
+    // Remove web links
+    while (cleanedText.find("http") != std::string::npos) {
+        int start = cleanedText.find("http");
+        int end = cleanedText.find(" ", start);
+        cleanedText.erase(start, end - start);
+    }
+
+    // Remove all numbers
+    for (int i = 0; i < cleanedText.size(); i++) {
+        if (isdigit(cleanedText[i])) {
+            cleanedText.erase(i--, 1);
+        }
+    }
+
+    return cleanedText;
+};
+
+// Load CSV file
+bool loadCSV(const std::string& filename, std::vector<CSVRecord>& data, std::string textHeader, std::string flagHeader) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return false;
+    }
+
+    // Skip the first line (header)
+    std::string line;
+    std::getline(file, line);
+    std::stringstream ss(line);
+    std::string cell;
+
+    int textIndex = -1;
+    int flagIndex = -1;
+    int i = 0;
+
+    while (std::getline(ss, cell, ',')) {
+        if (cell == textHeader) {
+            textIndex = i;
+        } else if (cell == flagHeader) {
+            flagIndex = i;
+        }
+
+        i++;
+    }
+
+    if (textIndex == -1) {
+        std::cerr << "Text header not found: " << textHeader << std::endl;
+        return false;
+    }
+
+    if (flagIndex == -1) {
+        std::cerr << "Flag header not found: " << flagHeader << std::endl;
+        return false;
+    }
+
+    std::stringstream dataStream;
+
+    // Read the remaining data lines into a single stringstream
+    while (std::getline(file, line)) {
+        dataStream << line << " ,";
+    }
+
+    // Replace all occourances of "" with ''
+    std::string dataString = dataStream.str();
+
+    while (dataString.find("\"\"") != std::string::npos) {
+        dataString.replace(dataString.find("\"\""), 2, "\'\'");
+    }
+
+    dataStream = std::stringstream(dataString);
+
+    std::string token;
+    int tokenIndex = 0;
+
+    std::string text;
+    int label;
+
+    // Parse the data from the stringstream
+    while (std::getline(dataStream, token, ',')) {
+        if (token.front() == '"') {
+            std::string temp = token;
+            while (temp.back() != '"' && std::getline(dataStream, token, ',')) {
+                temp += " ," + token;
+            }
+            text = cleanText(temp);
+            // std::cout << text << std::endl;
+        }
+
+        if (tokenIndex % 4 == flagIndex) {
+            // std::cout << token << std::endl;
+            label = std::stoi(token);
+        }
+
+        // std::cout << tokenIndex << std::endl;
+
+        tokenIndex += 1;
+
+        if (label != -1) {
+            data.push_back(CSVRecord{text, label});
+            text = "";
+            label = -1;
+        }
+    }
+
+    file.close();
+    return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+#include "data.hpp"
+
 int main() {
 
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Program is running..." << std::endl;
+
+    std::vector<std::string> header = {"text", "label"};
+    std::vector<CSVRecord> data;
+    std::string textHeader = "text";
+    std::string flagHeader = "label_num";
+
+    loadCSV("../inc/kaggleDataset.csv", data, textHeader, flagHeader);
+
+    std::cout << "Data size: " << data.size() << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << data[i].text << std::endl;
+        std::cout << data[i].label << std::endl;
+    }
+
 
     return 0;
 }
