5658bbd39936e3bfeaae92822d61989762a06fcd
Will Ward <will007ward@gmail.com>
2023-11-07 21:19:16 +0000

    LSTM Test

--- dense.py
+++ dense.py
@@ -142,3 +142,103 @@ class Recurrent(Layer):
         self.recurrent_weights -= mul(recurrent_weights_gradient, learning_rate)
         self.bias -= mul(np.sum(output_gradient, axis=1, keepdims=True), learning_rate)
         return input_gradient
+
+# class tempSigmoid:
+#     def forward(self, x):
+#         return 1 / (1 + np.exp(-x))
+
+#     def backward(self, output_gradient, learning_rate):
+#         return output_gradient * (1 - output_gradient)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+class LSTM(Layer):
+    def __init__(self, input_size, output_size, hidden_size=100, activation=Sigmoid()):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.activation = activation
+
+        self.w_f = np.random.randn(hidden_size, hidden_size + input_size)
+        self.w_i = np.random.randn(hidden_size, hidden_size + input_size)
+        self.w_c = np.random.randn(hidden_size, hidden_size + input_size)
+        self.w_o = np.random.randn(hidden_size, hidden_size + input_size)
+        self.w_y = np.random.randn(output_size, hidden_size)
+
+        self.b_f = np.random.randn(hidden_size, 1)
+        self.b_i = np.random.randn(hidden_size, 1)
+        self.b_c = np.random.randn(hidden_size, 1)
+        self.b_o = np.random.randn(hidden_size, 1)
+        self.b_y = np.random.randn(output_size, 1)
+
+        self.f = np.zeros((hidden_size, 1))
+        self.i = np.zeros((hidden_size, 1))
+        self.c = np.zeros((hidden_size, 1))
+        self.o = np.zeros((hidden_size, 1))
+        self.h = np.zeros((hidden_size, 1))
+
+    def forward(self, input_value, hidden_state, cell_state):
+        self.input = input_value
+        self.h = hidden_state
+
+        self.f = sigmoid(self.w_f @ np.vstack((self.h, self.input)) + self.b_f)
+        self.i = sigmoid(self.w_i @ np.vstack((self.h, self.input)) + self.b_i)
+        self.c = np.tanh(self.w_c @ np.vstack((self.h, self.input)) + self.b_c)
+        self.o = sigmoid(self.w_o @ np.vstack((self.h, self.input)) + self.b_o)
+
+        self.cell_state = self.f * cell_state + self.i * self.c
+        self.h = self.o * np.tanh(self.cell_state)
+
+        self.output = self.w_y @ self.h + self.b_y
+        self.output = sigmoid(self.output)
+
+        return self.output, self.h, self.cell_state
+
+    def backward(self, output_gradient, learning_rate):
+        output_gradient = self.activation.backward(output_gradient, learning_rate)
+
+        self.output_gradient = output_gradient
+
+        self.w_y_gradient = self.output_gradient @ self.h.T
+        self.b_y_gradient = self.output_gradient
+
+        self.h_gradient = self.w_y.T @ self.output_gradient
+        self.o_gradient = self.h_gradient * np.tanh(self.cell_state)
+        self.cell_state_gradient = self.h_gradient * self.o * (1 - np.tanh(self.cell_state) ** 2)
+        self.c_gradient = self.cell_state_gradient * self.i
+        self.i_gradient = self.cell_state_gradient * self.c
+        self.f_gradient = self.cell_state_gradient * self.cell_state
+
+        self.w_o_gradient = self.o_gradient @ np.vstack((self.h, self.input)).T
+        self.b_o_gradient = self.o_gradient
+
+        self.w_c_gradient = self.c_gradient @ np.vstack((self.h, self.input)).T
+        self.b_c_gradient = self.c_gradient
+
+        self.w_i_gradient = self.i_gradient @ np.vstack((self.h, self.input)).T
+        self.b_i_gradient = self.i_gradient
+
+        self.w_f_gradient = self.f_gradient @ np.vstack((self.h, self.input)).T
+        self.b_f_gradient = self.f_gradient
+
+        self.h_gradient = self.w_o.T @ self.o_gradient + self.w_c.T @ self.c_gradient + self.w_i.T @ self.i_gradient + self.w_f.T @ self.f_gradient
+        self.input_gradient = self.w_o.T @ self.o_gradient + self.w_c.T @ self.c_gradient + self.w_i.T @ self.i_gradient + self.w_f.T @ self.f_gradient
+
+        self.w_y -= mul(self.w_y_gradient, learning_rate)
+        self.b_y -= mul(self.b_y_gradient, learning_rate)
+
+        self.w_o -= mul(self.w_o_gradient, learning_rate)
+        self.b_o -= mul(self.b_o_gradient, learning_rate)
+
+        self.w_c -= mul(self.w_c_gradient, learning_rate)
+        self.b_c -= mul(self.b_c_gradient, learning_rate)
+
+        self.w_i -= mul(self.w_i_gradient, learning_rate)
+        self.b_i -= mul(self.b_i_gradient, learning_rate)
+
+        self.w_f -= mul(self.w_f_gradient, learning_rate)
+        self.b_f -= mul(self.b_f_gradient, learning_rate)
+
+        return self.input_gradient, self.h_gradient, self.cell_state_gradient
