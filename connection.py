ebd35947e0e95acced113a01375232894238cb40
Will Ward <will007ward@gmail.com>
2023-11-16 22:57:56 +0000

    Connection Class

--- connection.py
+++ connection.py
@@ -1,135 +1,143 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
 import os
 import base64
-from google.oauth2.credentials import Credentials
 import pickle
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from bs4 import BeautifulSoup
 
-def authenticate():
+class Connection:
 
-    # If modifying these scopes, delete the file token.pickle
-    SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.labels', 'https://www.googleapis.com/auth/gmail.modify']
+    def __init__(self):
+        self.__SCOPES = [
+                            'https://www.googleapis.com/auth/gmail.readonly',
+                            'https://www.googleapis.com/auth/gmail.labels',
+                            'https://www.googleapis.com/auth/gmail.modify'
+                        ]
 
-    # The file at token_path stores the user's access and refresh tokens, and is
-    # created automatically when the authorisation flow completes for the first time
-    token_path = 'token.pickle'
+        self.__service = None
+        self.__credentials = None
 
-    # Credentials for the API
-    creds = None
+    def check_connected(self, reconnect=False):
 
-    # Check if token_path exists
-    if os.path.exists(token_path):
-        with open(token_path, 'rb') as token:
-            creds = pickle.load(token)
+        # Check for valid credentials and service or reconnect flag
+        if (not self.__credentials or not self.__credentials.valid) or (not self.__service) or (reconnect):
 
-    # If there are no (valid) credentials available, let the user log in
-    if not creds or not creds.valid:
+            # Refresh credentials
+            self.update_credentials()
 
-        # Use 0Auth2.0 flow to generate credentials
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=8080)
+            # Rebuild service
+            self.__service = build('gmail', 'v1', credentials=self.__credentials)
 
-        # Save the credentials for next time
-        with open(token_path, 'wb') as token:
-            pickle.dump(creds, token)
+    def update_credentials(self):
 
-    # Return the authenticated Gmail API credentials
-    return creds
+        # The file at token_path stores the user's access and refresh tokens, and is
+        # created automatically when the authorisation flow completes for the first time
+        token_path = 'token.pickle'
 
-def get_user_emails(service):
+        # Credentials for the API
+        self.__credentials = None
 
-    # Get a list of messages
-    result = service.users().messages().list(userId='me').execute()
+        # Check if token_path exists
+        if os.path.exists(token_path):
+            with open(token_path, 'rb') as token:
+                self.__credentials = pickle.load(token)
 
-    # Get the messages from the result
-    messages = result.get('messages', [])
+        # If there are no (valid) credentials available, let the user log in
+        if not self.__credentials or not self.__credentials.valid:
 
-    # Return the messages
-    return messages
+            # Use 0Auth2.0 flow to generate credentials
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', self.__SCOPES)
+            self.__credentials = flow.run_local_server(port=8080)
 
-def get_email_content(service, message_id):
-    message = service.users().messages().get(userId='me', id=message_id).execute()
-    payload = message['payload']
-    headers = payload['headers']
-    subject = [header['value'] for header in headers if header['name'] == 'Subject'][0]
+            # Save the credentials for next time
+            with open(token_path, 'wb') as token:
+                pickle.dump(self.__credentials, token)
 
-    # The email body is in the 'data' field of the 'body' property of the 'payload'
-    # TODO: KeyError: 'data'
-    body_data = payload['body']['data']
-    body_text = base64.urlsafe_b64decode(body_data).decode('utf-8')
+    def get_user_emails(self):
+        self.check_connected()
 
-    # Parse HTML content using BeautifulSoup to extract plaintext
-    soup = BeautifulSoup(body_text, 'html.parser')
-    plaintext_content = soup.get_text(separator='\n')
+        # Get a list of messages
+        result = self.__service.users().messages().list(userId='me').execute()
 
-    # Remove empty lines
-    plaintext_content = '\n'.join([line for line in plaintext_content.split('\n') if line.strip()])
+        # Get the messages from the result
+        messages = result.get('messages', [])
 
-    # Remove no alphanumeric characters
-    plaintext_content = ' '.join(x if x.isalnum() else "" for x in plaintext_content.split())
+        # Return the messages
+        return messages
 
-    # Remove duplicate spaces
-    plaintext_content = ' '.join(plaintext_content.split())
+    def get_email_content(self, message_id):
+        self.check_connected()
 
-    # Remove leading and trailing spaces
-    plaintext_content = plaintext_content.strip()
+        message = self.__service.users().messages().get(userId='me', id=message_id).execute()
+        payload = message['payload']
+        headers = payload['headers']
+        subject = [header['value'] for header in headers if header['name'] == 'Subject'][0]
 
-    # Lowercase
-    plaintext_content = plaintext_content.lower()
+        # The email body is in the 'data' field of the 'body' property of the 'payload'
+        # TODO: Test if this is always the case
+        # body_data = payload['body']['data']
+        body_data = payload['parts'][0]['body']['data']
+        body_text = base64.urlsafe_b64decode(body_data).decode('utf-8')
 
-    print("Subject:", subject)
-    print(f"Plaintext Content:\n{plaintext_content}")
+        # Parse HTML content using BeautifulSoup to extract plaintext
+        soup = BeautifulSoup(body_text, 'html.parser')
+        plaintext_content = soup.get_text(separator='\n')
 
-def get_email_labels(service, message_id):
+        # TODO: Check that it is not removing text connected to a symbol e.g. Full stop, comma, etc.
 
-    # Get the message
-    result = service.users().messages().get(userId='me', id=message_id).execute()
+        # Remove empty lines
+        plaintext_content = '\n'.join([line for line in plaintext_content.split('\n') if line.strip()])
 
-    # Get the labels
-    labels = result.get('labelIds', [])
+        # Remove no alphanumeric characters
+        plaintext_content = ' '.join(x if x.isalnum() else "" for x in plaintext_content.split())
 
-    # Return the labels
-    return labels
+        # Remove duplicate spaces
+        plaintext_content = ' '.join(plaintext_content.split())
 
-def assign_email_labels(service, message_id, labels):
+        # Remove leading and trailing spaces
+        plaintext_content = plaintext_content.strip()
 
-    # Get the existing labels
-    existing_labels = service.users().labels().list(userId='me').execute()
+        # Lowercase
+        plaintext_content = plaintext_content.lower()
 
-    # Get the label IDs
-    label_ids = [label['id'] for label in existing_labels['labels'] if label['name'] in labels]
+        print("Subject:", subject)
+        print(f"Plaintext Content:\n{plaintext_content}")
 
-    # Assign the labels to a request body
-    body = {'removeLabelIds': [], 'addLabelIds': label_ids}
+    def get_email_labels(self, message_id):
+        self.check_connected()
 
-    # Modify the message
-    service.users().messages().modify(userId='me', id=message_id, body=body).execute()
+        # Get the message
+        result = self.__service.users().messages().get(userId='me', id=message_id).execute()
+
+        # Get the labels
+        labels = result.get('labelIds', [])
+
+        # Return the labels
+        return labels
+
+    def assign_email_labels(self, message_id, labels):
+        self.check_connected()
+
+        # Get the existing labels
+        existing_labels = self.__service.users().labels().list(userId='me').execute()
+
+        # Get the label IDs
+        label_ids = [label['id'] for label in existing_labels['labels'] if label['name'] in labels]
+
+        # Assign the labels to a request body
+        body = {'removeLabelIds': [], 'addLabelIds': label_ids}
+
+        # Modify the message
+        self.__service.users().messages().modify(userId='me', id=message_id, body=body).execute()
 
 # Main execution
 if __name__ == "__main__":
-    # Authenticate
-    credentials = authenticate()
-    service = build('gmail', 'v1', credentials=credentials)
+    conn = Connection()
 
-    # Get user emails
-    user_emails = get_user_emails(service)
-    # print("User Emails:", user_emails)
+    # Get the user's emails
+    messages = conn.get_user_emails()
 
-    # Choose a message_id from user_emails
-    if user_emails:
-        message_id = user_emails[0]['id']
-
-        # Get email labels
-        # email_labels = get_email_labels(service, message_id)
-        # print("Email Labels:", email_labels)
-
-        # Assign email labels
-        # label_ids = ['Safe']
-        # assign_email_labels(service, message_id, label_ids)
-        # print("Labels assigned successfully.")
-
-        # Get email content
-        get_email_content(service, message_id)
+    # Get the content of the first email
+    conn.get_email_content(messages[0]['id'])
