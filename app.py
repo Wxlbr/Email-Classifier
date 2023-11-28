6b51e112a26c454bd7b8d8ccefae3b2892a1f6aa
Will Ward <will007ward@gmail.com>
2023-11-28 21:40:23 +0000

    Added Navbar

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ layers = {}
 def index():
     return render_template('index.html')
 
+@app.route('/settings')
+def settings():
+    # Add your settings logic here
+    return render_template('settings.html')
+
 @app.route('/save_layer', methods=['POST'])
 def save_layer():
     data = request.get_json()
@@ -17,15 +22,29 @@ def save_layer():
     input_size = data.get('inputSize')
     output_size = data.get('outputSize')
 
+    if layer_id is None:
+        return jsonify({'code': '400', 'message': 'Layer ID cannot be empty'})
+    if input_size is None:
+        return jsonify({'code': '400', 'message': 'Input size cannot be empty'})
+    if output_size is None:
+        return jsonify({'code': '400', 'message': 'Output size cannot be empty'})
+
+    # TODO: Type checks for layer_id, input_size and output_size
+    input_size = int(input_size)
+    output_size = int(output_size)
+
     layers[layer_id] = {
         'inputSize': input_size,
         'outputSize': output_size
     }
 
-    print(layers)
+    # TODO: Checks for valid layer structure
+    if input_size == 0:
+        return jsonify({'code': '400', 'message': 'Matrix size cannot be zero'})
+    if output_size == 0:
+        return jsonify({'code': '400', 'message': 'Matrix size cannot be zero'})
 
-    # For demonstration purposes, returning the saved layers
-    return jsonify({'code': '201'})
+    return jsonify({'code': '200', 'message': 'Layer saved'})
 
 if __name__ == '__main__':
     app.run(debug=True)
