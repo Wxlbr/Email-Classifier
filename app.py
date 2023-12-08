1945d048e1069a81efc3678230dbcad2b08d2a9d
Will Ward <will007ward@gmail.com>
2023-12-08 18:24:32 +0000

    AddNetwork() and Page Communication Testing

--- app.py
+++ app.py
@@ -10,56 +10,21 @@ layers = {}
 def index():
     return render_template('index.html')
 
-@app.route('/networkDesigner')
-def networks():
-    # Add networks logic here
-    return render_template('networkDesigner.html')
+# Temp json
+data = {
+    'item': 'value'
+}
 
-@app.route('/save_layer', methods=['POST'])
-def save_layer():
+@app.route('/edit-network', methods=['GET'])
+def edit_network():
+    return render_template('index2.html', data=data)
+
+@app.route('/save-network', methods=['POST'])
+def save_network():
+    # Get data from request
     data = request.get_json()
-    layer_id = data.get('layerId')
-    input_size = data.get('inputSize')
-    output_size = data.get('outputSize')
-
-    if layer_id is None:
-        return jsonify({'code': '400', 'message': 'Layer ID cannot be empty'})
-    if input_size is None:
-        return jsonify({'code': '400', 'message': 'Input size cannot be empty'})
-    if output_size is None:
-        return jsonify({'code': '400', 'message': 'Output size cannot be empty'})
-
-    # TODO: Type checks for layer_id, input_size and output_size
-    input_size = int(input_size)
-    output_size = int(output_size)
-
-    layers[layer_id] = {
-        'inputSize': input_size,
-        'outputSize': output_size
-    }
-
-    # TODO: Checks for valid layer structure
-    if input_size == 0:
-        return jsonify({'code': '400', 'message': 'Matrix size cannot be zero'})
-    if output_size == 0:
-        return jsonify({'code': '400', 'message': 'Matrix size cannot be zero'})
-
-    return jsonify({'code': '200', 'message': 'Layer saved'})
-
-@app.route('/remove_layer', methods=['POST'])
-def remove_layer():
-    data = request.get_json()
-    layer_id = data.get('layerId')
-
-    if layer_id is None:
-        return jsonify({'code': '400', 'message': 'Layer ID cannot be empty'})
-
-    if layer_id in layers:
-        del layers[layer_id]
-        return jsonify({'code': '200', 'message': 'Layer removed'})
-
-    return jsonify({'code': '400', 'message': 'Layer not found'})
-
+    print(data)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
