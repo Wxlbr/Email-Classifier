9cad2fa24fdf324824fcd8c5ab3c7031d18c67f2
Will Ward <will007ward@gmail.com>
2023-12-09 15:10:08 +0000

    Networks Save File && Saves Between Instances

--- app.py
+++ app.py
@@ -1,29 +1,65 @@
-# app.py
+import os
+import json
+
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+def save_networks_to_file(networks):
+    with open('./inc/networks.json', 'w', encoding='utf-8') as f:
+        json.dump(networks, f, indent=4)
 
-# List to store layer information
-layers = {}
+def load_networks_from_file():
+    if not os.path.exists('./inc/networks.json'):
+        return {}
+    with open('./inc/networks.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Temp json
-data = {
-    'item': 'value'
-}
-
 @app.route('/edit-network', methods=['GET'])
 def edit_network():
+
+    # Get network Id from request
+    network_id = request.args.get('networkId')
+
+    # Get network from list
+    networks = load_networks_from_file()
+
+    # Get network from list
+    if network_id in networks:
+        layers = networks[network_id]['layers']
+    else:
+        layers = {}
+
+    data = {
+        'networkId': network_id,
+        'layers': layers
+    }
+
     return render_template('index2.html', data=data)
 
 @app.route('/save-network', methods=['POST'])
 def save_network():
     # Get data from request
     data = request.get_json()
-    print(data)
+
+    network_id = data.get('networkId')
+    layers = data.get('layers')
+
+    # Get networks from file
+    networks = load_networks_from_file()
+
+    # Save network to list
+    if network_id not in networks:
+        networks[network_id] = {}
+    networks[network_id]['layers'] = layers
+
+    # Save network to file
+    save_networks_to_file(networks)
+
     return jsonify(data)
 
 
